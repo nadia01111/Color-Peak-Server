2987465e62be30a3143bcfa1e9a5a81f19681d23
Nadia <nadikorableva@gmail.com>
2024-07-24 10:42:51 -0400

    server changes

--- server.js
+++ server.js
@@ -1,66 +1,55 @@
 require('dotenv').config();
 
-// "use strict";
-
 const express = require("express");
 const cors = require('cors');
-
-
 const morgan = require("morgan");
-const PORT = 8000;
+
+const PORT = process.env.PORT || 8000;  // Use the port provided by Heroku
+
 const corsOptions = {
-  origin: 'http://localhost:3000', // Replace with your React app's URL
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',  // Use environment variable for CORS origin
   optionsSuccessStatus: 200
 };
 
-
 const {
-    getRandomePalette,
-    getAllPalettes,
-    getUserbyId,
-    createUserMongo,
-    saveGeneratedPalette,
-    getSavedPalettes,
-    savePaletteFromPicture
+  getRandomePalette,
+  getAllPalettes,
+  getUserbyId,
+  createUserMongo,
+  saveGeneratedPalette,
+  getSavedPalettes,
+  savePaletteFromPicture
 } = require("./handlers");
 
-const {colorRecognize,imgRecognize
-    } = require("./googleVisionApI")
+const { colorRecognize, imgRecognize } = require("./googleVisionApI");
 
+const app = express();
 
-express()
-.use(cors(corsOptions)) 
-.use(morgan("tiny"))
-.use(express.json({ limit: '10mb' }))
-.use(express.static("public"))
-.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, HEAD, GET, PUT, POST, DELETE",
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
+app.use(cors(corsOptions))
+  .use(morgan("tiny"))
+  .use(express.json({ limit: '10mb' }))
+  .use(express.static("public"))
+  .use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE",
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 /* Endpoints */
-.get("/api/randome-palette", getRandomePalette)
-.get("/api/palettes", getAllPalettes)
-.get("/api/users/:id", getUserbyId)
-.get("/api/fetch-my-palettes/:id",getSavedPalettes)
+app.get("/api/randome-palette", getRandomePalette)
+  .get("/api/palettes", getAllPalettes)
+  .get("/api/users/:id", getUserbyId)
+  .get("/api/fetch-my-palettes/:id", getSavedPalettes)
+  .post("/api/user/create-user", createUserMongo)
+  .post("/api/color-recognize", colorRecognize)
+  .post("/api/save-palette-from-picture", savePaletteFromPicture)
+  .patch("/api/save-palette", saveGeneratedPalette)
+  .get("*", (req, res) => {
+    res.status(404).json({ status: 404, message: "No endpoint found." });
+  });
 
-.post("/api/user/create-user", createUserMongo)
-.post("/api/color-recognize",colorRecognize)
-.post("/api/save-palette-from-picture", savePaletteFromPicture)
-
-.patch("/api/save-palette",saveGeneratedPalette)
-
-
-
-
-// catch-all
-.get("*", (req, res) => {
-return res.status(404).json({ status: 404, message: "No endpoint found." })
-})
-
-.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
